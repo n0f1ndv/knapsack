ba2a3460108efb46a98c07a81b2e7a0d3cb17651
radek <boguslawskiradek@gmail.com>
2025-11-28 22:13:46 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Binary Knapsack Problem with categories
+## Example usage:
+This command generates data for  given problem and writes it in json format:
+    `python3 src/main.py --generate-data json`
--- src/main.py
+++ src/main.py
@@ -1,20 +1,23 @@
 import sys
 
 from generate_data import *
+from knapsack import *
 
 # TODO:
 # * Add functions to solve instances of knapsack problem
 #       (Do the research about available options)
 # * Add error-handling
-
+# * Add bash script that calls this program
 def main():
     if len(sys.argv) == 1:
         print(
             "Usage: python3 src/main.py <option>",
             "Available options:",
-            "--generate-data <file-type> - generates random input data based on specified ",
+            "--generate-data <file-type> - generates random input data based on specified",
             "min and max value given by user then saves it in a json file",
             "   available file types: dzn, json",
+            "--solve <option> - ...",
+            "   available options: greedy, ...",
             sep='\n'
             )
     else:
@@ -31,6 +34,19 @@ def main():
             generate_data_dzn(file_name, seed)
         elif sys.argv[2] == "json":
             generate_data_json(file_name, seed)
+        else:
+            print("Option was not recognized. Available options:")
+            exit(1)
+
+    if sys.argv[1] == "--solve":
+        print("WORK IN PROGRESS (:_-_;)\n")
+        if sys.argv[2] == "greedy":
+            greedy_knapsack()
+        if sys.argv[2] == "":
+            knapsack()
+        else:
+            print("Option was not recognized. Available options:")
+            exit(1)
 
 if __name__ == "__main__":
     main()
