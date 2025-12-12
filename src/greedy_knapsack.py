32380ab6051ed3571dbe8ff85817f1a0aa333a44
radek <boguslawskiradek@gmail.com>
2025-12-12 20:44:22 +0100

    Cleaned up greedy algorithm

--- src/greedy_knapsack.py
+++ src/greedy_knapsack.py
@@ -5,37 +5,44 @@ from generate_data import read_data_json
 # TODO: Add another methods of picking items (by weight, by ratio, by penalties)
 # Picking highest value items first
 def greedy_knapsack(data):
-    current_value = 0
-    current_weight = 0
+    final_value = 0
+    final_weight = 0
+    elements_used = [0] * data["items_number"]
+    used_elements_indices = []
     penalty_value = 0
 
-    best_weights = []
-    best_values = []
-    best_categories = []
+    values, weights, indices = zip(*sorted(zip(
+        data["values"],
+        data["weights"],
+        range(data["items_number"])), reverse=True)
+    )
 
-    while len(data["values"]):
-        max_value = max(data["values"])
-        max_value_index = data["values"].index(max_value)
+    for value, weight, index in zip(values, weights, indices):
+        final_value += value
+        final_weight += weight
+        elements_used[index] = 1
+        used_elements_indices.append(index)
 
-        if (current_weight + data["weights"][(max_value_index)] > data["capacity"]):
+        if (final_weight > data["capacity"]):
+            final_weight -= weight
+            final_value -= value
+            elements_used[index] = 0
+            used_elements_indices.remove(index)
             break
+
+    for i in range(len(used_elements_indices) - 1):
+        current_pos = used_elements_indices[i]
+        next_pos = used_elements_indices[i+1]
+
+        if (data["categories"][current_pos] == data["categories"][next_pos]):
+            continue
+
+        if (next_pos > current_pos):
+            key = f"{data["categories"][current_pos]}{data["categories"][next_pos]}"
         else:
-            best_weights.append(data["values"].pop(max_value_index))
-            best_values.append(data["weights"].pop(max_value_index))
-            best_categories.append(data["categories"].pop(max_value_index))
+            key = f"{data["categories"][current_pos]}{data["categories"][next_pos]}"
+        
+        print(data["values"][current_pos], data["values"][next_pos])
+        penalty_value += (data["penalties"][key] / 100) * (data["values"][current_pos] + data["values"][next_pos])
 
-            current_value += best_values[-1]
-            current_weight += best_weights[-1]
-
-        # print(f"Element used: weight = {best_weights[-1]}, value = {best_values[-1]}, category = {best_categories[-1]}") # DEBUG
-
-    for i in range(len(best_categories) - 1):
-        if (best_categories[i+1] != best_categories[i]):
-            if (best_categories[i+1] > best_categories[i]):
-                penalty_value += (data["penalties"][f"{best_categories[i]}{best_categories[i+1]}"] / 100) * (best_values[i] + best_values[i+1])
-            else:
-                penalty_value += (data["penalties"][f"{best_categories[i+1]}{best_categories[i]}"] / 100) * (best_values[i] + best_values[i+1])
-
-    final_value = current_value - penalty_value
-
-    return current_value, current_weight, penalty_value, final_value, None
+    return final_value, final_weight, penalty_value, final_value - penalty_value, elements_used
